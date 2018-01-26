f456a0380d7e0c4f8119cca9677daf3c4808a4b9
David Clark <primecomp@Davids-MacBook-Pro.local>
2018-01-26 14:03:35 -0600

    added amenities view change

--- server/public/scripts/services/survey.service.js
+++ server/public/scripts/services/survey.service.js
@@ -4,6 +4,7 @@ myApp.service('SurveyService', function($http, $location){
 
     self.demographics = { list: [] };
     self.location = { list: [] };
+    self.amenities = { list: [] };
 
 //request to populate demographic questions and possible answers.
     self.getDemographics = function () {
@@ -27,6 +28,19 @@ myApp.service('SurveyService', function($http, $location){
             console.log('response', response);
             for (let i = 0; i < response.data.length; i++) {
             self.location.list.push(response.data[i]);//this fills up the questions array with the table from the database.
+            }          
+        });
+    }
+
+//request to populate amenities questions and possible answers.
+    self.getAmenities = function () {
+        $http({
+            method: 'GET',
+            url: '/amenities'
+        }).then(function (response) {
+            console.log('response', response);
+            for (let i = 0; i < response.data.length; i++) {
+            self.amenities.list.push(response.data[i]);//this fills up the questions array with the table from the database.
             }          
         });
     }
