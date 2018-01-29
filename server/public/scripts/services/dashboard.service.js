97474e01368c4e060eb743b321b35ed2289225c8
Joe Wales <joewales13@gmail.com>
2018-01-29 15:04:42 -0600

    add chats.js and angular-charts, create text dashboard chart, update db.sql

--- server/public/scripts/services/dashboard.service.js
+++ server/public/scripts/services/dashboard.service.js
@@ -1,11 +1,24 @@
 myApp.service('DashboardService', function ($http, $location) {
     console.log('DashboardService Loaded');
     var vm = this;
+    vm.responseData = { list: [] };
+    
+
+    vm.getAgeGroups = function () {
+        $http({
+            method: 'GET',
+            url: '/dashboard',
+        }).then(function (response) {
+            vm.responseData.list = response.data;
+            console.log('response info:', vm.responseData.list)
+            vm.updateChart();
+        });
+    }//end getAgeGroups
 
 
 
 
-
+     
 
 
 }); //end service    
