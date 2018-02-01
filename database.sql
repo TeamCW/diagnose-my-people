b49ab6b05f1503b22ad22aaca7cfea49a5a8fd9e
Joe Wales <joewales13@gmail.com>
2018-02-01 09:32:28 -0600

    update sql file and admin page

--- database.sql
+++ database.sql
@@ -14,7 +14,6 @@ CREATE TABLE "questions" (
 	"id" serial NOT NULL,
 	"question" varchar NOT NULL,
 	"kpi_id" integer NOT NULL,
-	"style_id" integer NOT NULL,
 	CONSTRAINT questions_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -39,16 +38,6 @@ CREATE TABLE "possible_responses" (
 	"response_text" varchar NOT NULL,
 	"style_id" integer NOT NULL,
 	CONSTRAINT possible_responses_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "question_styles" (
-	"id" serial NOT NULL,
-	"display_style_type" varchar NOT NULL,
-	CONSTRAINT question_styles_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -97,9 +86,9 @@ CREATE TABLE "client" (
 	"organization" varchar,
 	"survey_hash" varchar UNIQUE,
 	"contact_number" varchar,
-	"position" varchar,
 	"status" varchar NOT NULL DEFAULT 'New Client',
 	"comments" varchar,
+	"logo_url" varchar,
 	CONSTRAINT client_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -109,12 +98,10 @@ CREATE TABLE "client" (
 
 
 ALTER TABLE "questions" ADD CONSTRAINT "questions_fk0" FOREIGN KEY ("kpi_id") REFERENCES "category"("id");
-ALTER TABLE "questions" ADD CONSTRAINT "questions_fk1" FOREIGN KEY ("style_id") REFERENCES "question_styles"("id");
 
 
 ALTER TABLE "possible_responses" ADD CONSTRAINT "possible_responses_fk0" FOREIGN KEY ("question_id") REFERENCES "questions"("id");
 ALTER TABLE "possible_responses" ADD CONSTRAINT "possible_responses_fk1" FOREIGN KEY ("style_id") REFERENCES "response_style"("id");
-
 
 
 ALTER TABLE "employee_results" ADD CONSTRAINT "employee_results_fk0" FOREIGN KEY ("client_id") REFERENCES "client"("id") ON DELETE CASCADE;
@@ -122,7 +109,9 @@ ALTER TABLE "employee_results" ADD CONSTRAINT "employee_results_fk1" FOREIGN KEY
 ALTER TABLE "employee_results" ADD CONSTRAINT "employee_results_fk2" FOREIGN KEY ("response_id") REFERENCES "possible_responses"("id");
 
 ALTER TABLE "selected_kpi" ADD CONSTRAINT "selected_kpi_fk0" FOREIGN KEY ("client_id") REFERENCES "client"("id") ON DELETE CASCADE;
-ALTER TABLE "selected_kpi" ADD CONSTRAINT "selected_kpi_fk1" FOREIGN KEY ("kpi_id") REFERENCES "category"("id");
+ALTER TABLE "selected_kpi" ADD CONSTRAINT "selected_kpi_fk1" FOREIGN KEY ("kpi_id") REFERENCES "category"("id") ON DELETE CASCADE;
+
+
 
 
 
